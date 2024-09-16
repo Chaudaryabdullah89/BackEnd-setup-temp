6895bd6ec6f0e8ddf91c169d81879b084afa4c9f
Chaudaryabdullah <chaudaryabdullah899@gmail.com>
2024-09-16 11:46:03 -0700

    chnages

--- src/app.js
+++ src/app.js
@@ -1,9 +1,29 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import express from "express";
 const app = express();
 
-app.get("/",(req,res)=>{
-    res.send("Hello World");
-})
 
-export default app;
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(express.json({ limit: "16kb" }));
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+app.use(express.json({ limit: "16kb" }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+
+    res.send("Hello World! i am a backend developer");
+});
+
+
+app.post("/", (req, res) => {
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
